087a329a485ea2f2a2b52cd20c94355a00e201b4
kaurmukhveer <mukhveer96@gmail.com>
2026-05-20 19:01:44 -0400

    initial commit- Set up personal portfolio landing page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="display-3 fw-bold">Hello, I’m Mukhveer</h1>
+      <p className="lead">Computer Science Student at University of Ottawa</p>
+      <button className = "btn btn-dark">View My Work</button>
+    </div>
+  )
+}
+
+export default App
